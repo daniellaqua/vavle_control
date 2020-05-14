10f00350a13a561c9e7a9cd6b17d6695c596f41b
Daniel Laqua <daniel.laqua@tu-ilmenau.de>
2020-05-14 20:27:11 +0200

    MQTT Werte an den

--- valve_control/valve_control.ino
+++ valve_control/valve_control.ino
@@ -5,11 +5,23 @@
 //  char array1[12] = {0xFF, 0xFF, 0x02, 0x80, 0x92, 0x05, 0x01, 0x42, 0x48, 0x11, 0x11, 0x1E};
 //  char array1[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+ 
+const char* SSID = "WLAN-314868";
+const char* PSK = "83191081396733674200";
+const char* MQTT_BROKER = "192.168.2.148";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 
 // constants won't change. They're used here to set pin numbers:
 const int buttonPin = D6;     // the number of the pushbutton pin
 int buttonState = 0;
 int pulsation = 0;
+int open_time = 444;
+int close_time = 555;
 
 //char receivedChar;
 //boolean newData = false;
@@ -19,23 +31,118 @@ int pulsation = 0;
 
 void ICACHE_RAM_ATTR ISRoutine ();
 
+void callback(String topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+    String openTemp;
+      String closeTemp;
+      
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  for (int i = 13; i < 16; i++) {
+    Serial.print((char)message[i]);
+    openTemp += (char)message[i];
+  }
+   Serial.println();
+  open_time=openTemp.toInt(); 
+    for (int i = 30; i < 33; i++) {
+    Serial.print((char)message[i]);
+    closeTemp += (char)message[i];
+    
+  }
+  close_time=closeTemp.toInt(); 
+ Serial.println();
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+//  // If a message is received on the topic room/lamp, you check if the message is either on or off. Turns the lamp GPIO according to the message
+//  if(topic=="Pumpensteuerung/Pumpe_Maternal"){
+//      Serial.print("switching pump: ");
+//      if(messageTemp == "on"){
+//        digitalWrite(pumpMaternal, HIGH);
+//        Serial.print("On");
+//        //client.publish("Pumpensteuerung/Pumpe_Maternal", "on");
+//        client.publish("Pumpensteuerung/Hinweis_Maternal", "Remote start!");
+//      }
+//      else if(messageTemp == "off"){
+//        digitalWrite(pumpMaternal, LOW);
+//        Serial.print("Off");
+//        //client.publish("Pumpensteuerung/Pumpe_Maternal", "off");
+//        client.publish("Pumpensteuerung/Hinweis_Maternal", "Remote stop!");
+//      }
+//  }
+  Serial.println();
+}
+
+
 void setup() {
   // initialize serial:
+  Serial.begin(115200);
   Serial1.begin(9600);
   // reserve 200 bytes for the inputString:
   //inputString.reserve(200);
+
+  setup_wifi();
+  client.setServer(MQTT_BROKER, 1883);
+  client.setCallback(callback);
 
   pinMode(buttonPin,INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(buttonPin),ISRoutine,FALLING); 
 }
 
+void setup_wifi() {
+    delay(10);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(SSID);
+ 
+    WiFi.begin(SSID, PSK);
+ 
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+ 
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+    while (!client.connected()) {
+        Serial.print("Reconnecting...");
+        if (!client.connect("ESP8266Client")) {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" retrying in 5 seconds");
+            delay(5000);
+        }
+        client.subscribe("Ventilsteuerung/Timing_Maternal");
+    }
+}
+
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  
   if (pulsation == 1){  
     //open50();
+    
     open100();
-    delay(500);
+    Serial.print(open_time);
+    delay(open_time);
+
+    Serial.print(close_time);
     close_valve();
-    delay(500);
+    delay(close_time);
   }
 }
 
